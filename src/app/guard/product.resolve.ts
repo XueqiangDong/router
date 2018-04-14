39494eaf18f4c54a26e12afdb8d0d8df0f857212
Xueqiang Dong <dxq7541@gmail.com>
2018-04-13 21:55:54 -0400

    final

---
+++ src/app/guard/product.resolve.ts
@@ -0,0 +1,21 @@
+import {ActivatedRouteSnapshot, Resolve, Router, RouterStateSnapshot} from '@angular/router';
+import {isUndefined} from 'util';
+import {Product} from '../product/product.component';
+import {Observable} from 'rxjs/Observable';
+import {Injectable} from '@angular/core';
+
+@Injectable()
+export class ProductResolve implements Resolve<Product> {
+  constructor(private router: Router) {
+  }
+
+  resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<Product> | Product {
+    let productId: number = route.params['id'];
+    if (productId == 2) {
+      return new Product(1, 'iPhone7');
+    } else {
+      this.router.navigate(['/home']);
+      return undefined;
+    }
+  }
+}
--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -8,17 +8,28 @@ import {ActivatedRoute, Params} from '@angular/router';
 })
 export class ProductComponent implements OnInit {
   protected productId: number;
+  protected productName: string;
 
   constructor(private routeInfo: ActivatedRoute) {
   }
 
   ngOnInit() {
     this.routeInfo.params.subscribe((params: Params) => this.productId = params['id']);
+    this.routeInfo.data.subscribe((data: { product: Product }) => {
+      this.productId = data.product.id;
+      this.productName = data.product.name;
+    });
     if (this.routeInfo.snapshot.queryParams['id'] == null) {
       this.productId = this.routeInfo.snapshot.params['id'];
     } else {
       this.productId = this.routeInfo.snapshot.queryParams['id'];
     }
+  }
+
+}
+
+export class Product {
+  constructor(public id: number, public name: string) {
   }
 
 }
